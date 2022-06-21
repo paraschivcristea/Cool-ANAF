438f61f65fecf77460335f07345b1e06b90acdb0
paraschivcristea <100124467+paraschivcristea@users.noreply.github.com>
2022-06-21 23:13:44 +0300

    Update

--- js/myscripts.js
+++ js/myscripts.js
@@ -9,7 +9,18 @@ document.getElementById("mySidenav").style.width = "0";
 })
 
 const collapseBtn = document.querySelector("#collapsebtn");
+const collapseBtnClose = document.querySelector("#collapsebtnclose");
+
 collapseBtn.addEventListener("click", () => {
     document.getElementById("collapse").style.display = "flex";
     document.getElementById("collapsebtn").scrollIntoView({behavior: "smooth", block: "center"});
+    document.getElementById("collapsebtn").style.display = "none";
+    document.getElementById("collapsebtnclose").style.display = "flex";
+})
+
+collapseBtnClose.addEventListener("click", () => {
+    document.getElementById("collapse").style.display = "none";
+    document.getElementById("collapsetitle").scrollIntoView({behavior: "smooth", block: "end"});
+    document.getElementById("collapsebtnclose").style.display = "none";
+    document.getElementById("collapsebtn").style.display = "flex";
 })
