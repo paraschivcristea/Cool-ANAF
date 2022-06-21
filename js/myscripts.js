c0c8e4f6fda42a4995a858bc0e026508e7869193
paraschivcristea <100124467+paraschivcristea@users.noreply.github.com>
2022-06-21 14:07:55 +0300

    Update

--- js/myscripts.js
+++ js/myscripts.js
@@ -7,3 +7,9 @@ const closeButton = document.querySelector(".closebtn");
 closeButton.addEventListener("click", () => {
 document.getElementById("mySidenav").style.width = "0";
 })
+
+const collapseBtn = document.querySelector("#collapsebtn");
+collapseBtn.addEventListener("click", () => {
+    document.getElementById("collapse").style.display = "flex";
+    document.getElementById("collapsebtn").scrollIntoView({behavior: "smooth", block: "center"});
+})
