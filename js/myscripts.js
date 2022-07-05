3e49e30eafc095db50999a66e737ebafd1a8e664
paraschivcristea <100124467+paraschivcristea@users.noreply.github.com>
2022-07-05 12:15:44 +0300

    Update

--- js/myscripts.js
+++ js/myscripts.js
@@ -1,115 +1,17 @@
-const menuToggle = document.querySelector(".menu-bars");
-const nav = document.querySelector("nav ul");
-
-menuToggle.addEventListener("click", () => {
-  nav.classList.toggle("slide");
-  });
-
-  (function() {
-    $(document).ready(function() {
-      var walkthrough;
-      walkthrough = {
-        index: 0,
-        nextScreen: function() {
-          if (this.index < this.indexMax()) {
-            this.index++;
-            return this.updateScreen();
-          }
-        },
-        prevScreen: function() {
-          if (this.index > 0) {
-            this.index--;
-            return this.updateScreen();
-          }
-        },
-        updateScreen: function() {
-          this.reset();
-          this.goTo(this.index);
-          return this.setBtns();
-        },
-        setBtns: function() {
-          var $lastBtn, $nextBtn, $prevBtn;
-          $nextBtn = $('.next-screen');
-          $prevBtn = $('.prev-screen');
-          $lastBtn = $('.finish');
-          if (walkthrough.index === walkthrough.indexMax()) {
-            $nextBtn.prop('disabled', true);
-            $prevBtn.prop('disabled', false);
-            return $lastBtn.addClass('active').prop('disabled', false);
-          } else if (walkthrough.index === 0) {
-            $nextBtn.prop('disabled', false);
-            $prevBtn.prop('disabled', true);
-            return $lastBtn.removeClass('active').prop('disabled', true);
-          } else {
-            $nextBtn.prop('disabled', false);
-            $prevBtn.prop('disabled', false);
-            return $lastBtn.removeClass('active').prop('disabled', true);
-          }
-        },
-        goTo: function(index) {
-          $('.screen').eq(index).addClass('active');
-          return $('.dot').eq(index).addClass('active');
-        },
-        reset: function() {
-          return $('.screen, .dot').removeClass('active');
-        },
-        indexMax: function() {
-          return $('.screen').length - 1;
-        },
-        closeModal: function() {
-          $('.walkthrough, .shade').removeClass('reveal');
-          return setTimeout((() => {
-            $('.walkthrough, .shade').removeClass('show');
-            this.index = 0;
-            return this.updateScreen();
-          }), 200);
-        },
-        openModal: function() {
-          $('.walkthrough, .shade').addClass('show');
-          setTimeout((() => {
-            return $('.walkthrough, .shade').addClass('reveal');
-          }), 200);
-          return this.updateScreen();
+document.addEventListener('click', event => {
+    if (event.target.closest('[data-contrast]')) {
+        const contrast = window.sessionStorage.getItem('contrast') || '';
+        
+        if (contrast === 'high') {
+            window.sessionStorage.removeItem('contrast');
+            document.body.classList.remove('high-contrast');
+            menu.classList.remove('mm-menu--theme-white-contrast');
+            menu.classList.add('mm-menu--theme-white');
+        } else {
+            window.sessionStorage.setItem('contrast', 'high');
+            document.body.classList.add('high-contrast');
+            menu.classList.add('mm-menu--theme-white-contrast');
+            menu.classList.remove('mm-menu--theme-white');
         }
-      };
-      $('.next-screen').click(function() {
-        return walkthrough.nextScreen();
-      });
-      $('.prev-screen').click(function() {
-        return walkthrough.prevScreen();
-      });
-      $('.close').click(function() {
-        return walkthrough.closeModal();
-      });
-      $('.open-walkthrough').click(function() {
-        return walkthrough.openModal();
-      });
-      walkthrough.openModal();
-      
-      // Optionally use arrow keys to navigate walkthrough
-      return $(document).keydown(function(e) {
-        switch (e.which) {
-          case 37:
-            // left
-            walkthrough.prevScreen();
-            break;
-          case 38:
-            // up
-            walkthrough.openModal();
-            break;
-          case 39:
-            // right
-            walkthrough.nextScreen();
-            break;
-          case 40:
-            // down
-            walkthrough.closeModal();
-            break;
-          default:
-            return;
-        }
-        e.preventDefault();
-      });
-    });
-  
-  }).call(this);
+    }
+});
