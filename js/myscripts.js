89541da8520c3020592e244b9c5709965822b374
paraschivcristea <100124467+paraschivcristea@users.noreply.github.com>
2022-07-06 21:27:37 +0300

    update

--- js/myscripts.js
+++ js/myscripts.js
@@ -1,83 +1,212 @@
-const toggle = document.querySelector(".toggle");
-const menu = document.querySelector(".menu");
-const items = document.querySelectorAll(".item");
-
-/* Toggle mobile menu */
-function toggleMenu() {
-  if (menu.classList.contains("active")) {
-    menu.classList.remove("active");
-    toggle.querySelector("a").innerHTML = "<i class='fas fa-bars'></i>";
-  } else {
-    menu.classList.add("active");
-    toggle.querySelector("a").innerHTML = "<i class='fas fa-times'></i>";
-  }
+const menuBtn = document.querySelector(".menu-icon span");
+const searchBtn = document.querySelector(".search-icon");
+const cancelBtn = document.querySelector(".cancel-icon");
+const items = document.querySelector(".nav-items");
+const form = document.querySelector("form");
+menuBtn.onclick = ()=>{
+  items.classList.add("active");
+  menuBtn.classList.add("hide");
+  searchBtn.classList.add("hide");
+  cancelBtn.classList.add("show");
+}
+cancelBtn.onclick = ()=>{
+  items.classList.remove("active");
+  menuBtn.classList.remove("hide");
+  searchBtn.classList.remove("hide");
+  cancelBtn.classList.remove("show");
+  form.classList.remove("active");
+  cancelBtn.style.color = "#ff3d00";
+}
+searchBtn.onclick = ()=>{
+  form.classList.add("active");
+  searchBtn.classList.add("hide");
+  cancelBtn.classList.add("show");
 }
 
-/* Activate Submenu */
-function toggleItem() {
-  if (this.classList.contains("submenu-active")) {
-    this.classList.remove("submenu-active");
-  } else if (menu.querySelector(".submenu-active")) {
-    menu.querySelector(".submenu-active").classList.remove("submenu-active");
-    this.classList.add("submenu-active");
-  } else {
-    this.classList.add("submenu-active");
-  }
-}
+(() => {
+  // Respect user perference
+  const isReducedMotion = window.matchMedia("(prefers-reduced-motion: reduce)")
+    .matches;
 
-/* Close Submenu From Anywhere */
-function closeSubmenu(e) {
-  if (menu.querySelector(".submenu-active")) {
-    let isClickInside = menu
-      .querySelector(".submenu-active")
-      .contains(e.target);
-
-    if (!isClickInside && menu.querySelector(".submenu-active")) {
-      menu.querySelector(".submenu-active").classList.remove("submenu-active");
-    }
-  }
-}
-/* Event Listeners */
-toggle.addEventListener("click", toggleMenu, false);
-for (let item of items) {
-  if (item.querySelector(".submenu")) {
-    item.addEventListener("click", toggleItem, false);
-  }
-  item.addEventListener("keypress", toggleItem, false);
-}
-document.addEventListener("click", closeSubmenu, false);
-
-$(document).ready(function () {
-  var zindex = 10;
-
-  $("div.card").click(function (e) {
-    e.preventDefault();
-
-    var isShowing = false;
-
-    if ($(this).hasClass("show")) {
-      isShowing = true;
-    }
-
-    if ($("div.cards").hasClass("showing")) {
-      // a card is already in view
-      $("div.card.show").removeClass("show");
-
-      if (isShowing) {
-        // this card was showing - reset the grid
-        $("div.cards").removeClass("showing");
+  // Helper to apply inline CSS
+  const setStyleProps = ($el, styles) => {
+    for (const [key, value] of Object.entries(styles)) {
+      if (value === false) {
+        $el.style.removeProperty(key);
       } else {
-        // this card isn't showing - get in with it
-        $(this).css({ zIndex: zindex }).addClass("show");
+        $el.style.setProperty(key, value);
       }
-
-      zindex++;
-    } else {
-      // no cards in view
-      $("div.cards").addClass("showing");
-      $(this).css({ zIndex: zindex }).addClass("show");
-
-      zindex++;
     }
+  };
+
+  document.querySelectorAll(".Carousel").forEach(($carousel) => {
+    $carousel.scrollLeft = 0;
+
+    const $cards = Array.from($carousel.querySelectorAll(".Card"));
+    const $pagination = $carousel.nextElementSibling;
+    const [$previous, $next] = $pagination.querySelectorAll(".Arrow");
+    $pagination.querySelector(".Dot").classList.add("Dot--active");
+
+    const $start = document.createElement("div");
+    const $end = document.createElement("div");
+    $carousel.prepend($start);
+    $carousel.append($end);
+
+    const observer = new IntersectionObserver((entries) => {
+      entries.forEach((entry) => {
+        if (entry.target === $start) {
+          if ($previous) {
+            $previous.disabled = entry.isIntersecting;
+          }
+        }
+        if (entry.target === $end) {
+          if ($next) {
+            $next.disabled = entry.isIntersecting;
+          }
+        }
+      });
+    });
+    observer.observe($start);
+    observer.observe($end);
+
+    const scrollTo = ($card) => {
+      let offset = getOffset($card);
+      const left = document.dir === "rtl" ? -offset : offset;
+      const behavior = isReducedMotion ? "auto" : "smooth";
+      $carousel.scrollTo({ left, behavior });
+    };
+
+    const getOffset = ($el) => {
+      let offset = $el.offsetLeft;
+      if (document.dir === "rtl") {
+        offset = $carousel.offsetWidth - (offset + $el.offsetWidth);
+      }
+      return offset;
+    };
+
+    $previous.addEventListener("click", (ev) => {
+      ev.preventDefault();
+      let $card = $cards[0];
+      const scroll = Math.abs($carousel.scrollLeft);
+      $cards.forEach(($item) => {
+        const offset = getOffset($item);
+        if (offset - scroll < -1 && offset > getOffset($card)) {
+          $card = $item;
+        }
+      });
+      scrollTo($card);
+    });
+
+    $next.addEventListener("click", (ev) => {
+      ev.preventDefault();
+      let $card = $cards[$cards.length - 1];
+      const scroll = Math.abs($carousel.scrollLeft);
+      $cards.forEach(($item) => {
+        const offset = getOffset($item);
+        if (offset - scroll > 1 && offset < getOffset($card)) {
+          $card = $item;
+        }
+      });
+      scrollTo($card);
+    });
+
+    $pagination.addEventListener("click", (ev) => {
+      if (ev.target.classList.contains("Dot")) {
+        ev.preventDefault();
+        const $card = document.querySelector(new URL(ev.target.href).hash);
+        if ($card) scrollTo($card);
+      }
+    });
+
+    // Highlight nearest "Dot" in pagination
+    let scrollTimeout;
+    $carousel.addEventListener(
+      "scroll",
+      () => {
+        clearTimeout(scrollTimeout);
+        scrollTimeout = setTimeout(() => {
+          let $dot = $pagination.querySelector(".Dot--active");
+          if ($dot) $dot.classList.remove("Dot--active");
+          let $active;
+          const scroll = Math.abs($carousel.scrollLeft);
+          if (scroll <= 0) {
+            $active = $cards[0];
+          }
+          if (scroll >= $carousel.scrollWidth - $carousel.offsetWidth) {
+            $active = $cards[$cards.length - 1];
+          }
+          if (!$active) {
+            let oldDiff;
+            $cards.forEach(($card) => {
+              const newDiff = Math.abs(scroll - getOffset($card));
+              if (!$active || newDiff < oldDiff) {
+                $active = $card;
+                oldDiff = newDiff;
+              }
+            });
+          }
+          $dot = $pagination.querySelector(
+            `[href="#${($active ?? $card[0]).id}"]`
+          );
+          if ($dot) $dot.classList.add("Dot--active");
+        }, 50);
+      },
+      { passive: true }
+    );
+
+    // Improve arrow key navigation
+    $carousel.addEventListener("keydown", (ev) => {
+      if (/^(Arrow)?Left$/.test(ev.key)) {
+        ev.preventDefault();
+        (document.dir === "rtl" ? $next : $previous).click();
+      } else if (/(Arrow)?Right$/.test(ev.key)) {
+        ev.preventDefault();
+        (document.dir === "rtl" ? $previous : $next).click();
+      }
+    });
+
+    // Improve transition when tabbing focus
+
+    let scrollLeft = 0;
+    $carousel.addEventListener(
+      "blur",
+      (ev) => {
+        scrollLeft = $carousel.scrollLeft;
+      },
+      { capture: true }
+    );
+    $carousel.addEventListener(
+      "focus",
+      (ev) => {
+        $carousel.scrollLeft = scrollLeft;
+        const $card = ev.target.closest(".Card");
+        if ($card) scrollTo($card);
+      },
+      { capture: true }
+    );
   });
-});
+
+  // Optional polyfill for Safari 14
+  if (!isReducedMotion && !window.CSS.supports("scroll-behavior: smooth")) {
+    import("https://cdn.skypack.dev/smoothscroll-polyfill").then((module) => {
+      module.polyfill();
+    });
+  }
+})();
+
+(() => {
+  // Toggle right-to-left for demo
+  document.querySelector("#toggle-rtl").addEventListener("change", (ev) => {
+    document.dir = ev.target.checked ? "rtl" : "ltr";
+    document.querySelectorAll(".Carousel").forEach(($carousel) => {
+      $carousel.scrollLeft = 0;
+    });
+  });
+
+  // Toggle single slides class for demo
+  document.querySelector("#toggle-single").addEventListener("change", (ev) => {
+    document.querySelectorAll(".Carousel").forEach(($carousel) => {
+      $carousel.classList.toggle("Carousel--single", ev.target.checked);
+    });
+  });
+})();
