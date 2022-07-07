bc27e2477163b53f0d03bdb9394a2111c8404c59
paraschivcristea <100124467+paraschivcristea@users.noreply.github.com>
2022-07-07 23:45:34 +0300

    Update

--- js/myscripts.js
+++ js/myscripts.js
@@ -15,10 +15,31 @@ cancelBtn.onclick = ()=>{
   searchBtn.classList.remove("hide");
   cancelBtn.classList.remove("show");
   form.classList.remove("active");
-  cancelBtn.style.color = "white";
+  cancelBtn.style.color = "#7b1fa2;";
 }
 searchBtn.onclick = ()=>{
   form.classList.add("active");
   searchBtn.classList.add("hide");
   cancelBtn.classList.add("show");
+}
+
+window.addEventListener('scroll',(e)=>{
+  const nav = document.querySelector('header');
+  if(window.pageYOffset>0){
+    nav.classList.add("menushadow");
+  }else{
+    nav.classList.remove("menushadow");
+  }
+});
+
+var fullImg = document.querySelector(".full-img");
+var miniImg = document.querySelectorAll(".mini-img");
+
+function changeImg(x) {
+
+    var targetImg = miniImg[x - 1];
+    var imgAttr = targetImg.getAttribute("src");
+
+    fullImg.setAttribute("src", imgAttr);
+
 }
