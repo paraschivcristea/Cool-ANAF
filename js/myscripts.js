be37cbe842f7b2bd45c12446c49cc3ea2993dbf3
paraschivcristea <100124467+paraschivcristea@users.noreply.github.com>
2022-07-08 12:35:54 +0300

    Update

--- js/myscripts.js
+++ js/myscripts.js
@@ -32,14 +32,15 @@ window.addEventListener('scroll',(e)=>{
   }
 });
 
-var fullImg = document.querySelector(".full-img");
-var miniImg = document.querySelectorAll(".mini-img");
-
-function changeImg(x) {
-
-    var targetImg = miniImg[x - 1];
-    var imgAttr = targetImg.getAttribute("src");
-
-    fullImg.setAttribute("src", imgAttr);
-
-}
+const swiper = new Swiper('.swiper', {
+  spaceBetween: 30,
+  effect: "fade",
+  loop: true,
+  mousewheel: {
+    invert: false,
+  },
+  pagination: {
+    el: ".swiper-pagination",
+    clickable: true,
+  },
+});
