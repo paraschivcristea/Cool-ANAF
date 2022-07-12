eb47ccd28b2832856140dffbd405db492e098f81
paraschivcristea <100124467+paraschivcristea@users.noreply.github.com>
2022-07-12 13:39:03 +0300

    Update

--- js/myscripts.js
+++ js/myscripts.js
@@ -133,3 +133,26 @@ $(document).ready(function(){
     
   });
 });
+
+const swiper = new Swiper('.swiper', {
+  // Optional parameters
+  direction: 'vertical',
+  loop: true,
+
+  // If we need pagination
+  pagination: {
+    el: '.swiper-pagination',
+    type: 'bullets',
+  },
+
+  // Navigation arrows
+  navigation: {
+    nextEl: '.swiper-button-next',
+    prevEl: '.swiper-button-prev',
+  },
+
+  // And if we need scrollbar
+  scrollbar: {
+    el: '.swiper-scrollbar',
+  },
+});
