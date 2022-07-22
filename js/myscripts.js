b91da46ea8f605697547cb599ea7a82443c1dce1
paraschivcristea <100124467+paraschivcristea@users.noreply.github.com>
2022-07-22 21:30:31 +0300

    Update

--- js/myscripts.js
+++ js/myscripts.js
@@ -84,3 +84,14 @@ card4.addEventListener("mouseout", (e) => {
   cardBackground4.classList.remove("card-background-hover");
   cardTitle4.classList.remove("card-title-hover");
 });
+
+$(document).ready(function() {
+  $('.collapse.in').prev('.panel-heading').addClass('active');
+  $('#accordion, #bs-collapse')
+    .on('show.bs.collapse', function(a) {
+      $(a.target).prev('.panel-heading').addClass('active');
+    })
+    .on('hide.bs.collapse', function(a) {
+      $(a.target).prev('.panel-heading').removeClass('active');
+    });
+});
